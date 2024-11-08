6c093784edc138094b9e393169bf1e4e74a0fb0b
gabryelns <gabryelnick91@gmail.com>
2024-11-08 17:49:49 -0300

    adiciona problem 1931 - 100%

--- problem1931.c
+++ problem1931.c
@@ -16,12 +16,6 @@ typedef struct {
     int paridade;
 } No;
 
-int comparador(const void *a, const void *b) {
-    No *n1 = (No *)a;
-    No *n2 = (No *)b;
-    return n1->custo - n2->custo;
-}
-
 Estrada *adj[MAXN];
 int grau[MAXN];
 
@@ -33,15 +27,75 @@ void inicializaGrafo(int n) {
 }
 
 void adicionaEstrada(int u, int v, int pedagio) {
-    adj[u] = realloc(adj[u], (grau[u] + 1) * sizeof(Estrada));
+    Estrada *novoAdjU = realloc(adj[u], (grau[u] + 1) * sizeof(Estrada));
+    if (novoAdjU == NULL) {
+        fprintf(stderr, "Erro de alocação para adj[%d]\n", u);
+        exit(EXIT_FAILURE);
+    }
+    adj[u] = novoAdjU;
     adj[u][grau[u]].cidade = v;
     adj[u][grau[u]].pedagio = pedagio;
     grau[u]++;
 
-    adj[v] = realloc(adj[v], (grau[v] + 1) * sizeof(Estrada));
+    Estrada *novoAdjV = realloc(adj[v], (grau[v] + 1) * sizeof(Estrada));
+    if (novoAdjV == NULL) {
+        fprintf(stderr, "Erro de alocação para adj[%d]\n", v);
+        exit(EXIT_FAILURE);
+    }
+    adj[v] = novoAdjV;
     adj[v][grau[v]].cidade = u;
     adj[v][grau[v]].pedagio = pedagio;
     grau[v]++;
+}
+
+void swap(No *a, No *b) {
+    No temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void heapifyUp(No *heap, int index) {
+    while (index > 0) {
+        int parent = (index - 1) / 2;
+        if (heap[parent].custo <= heap[index].custo) break;
+        swap(&heap[parent], &heap[index]);
+        index = parent;
+    }
+}
+
+void heapifyDown(No *heap, int size, int index) {
+    while (2 * index + 1 < size) {
+        int left = 2 * index + 1;
+        int right = 2 * index + 2;
+        int smallest = index;
+
+        if (heap[left].custo < heap[smallest].custo) smallest = left;
+        if (right < size && heap[right].custo < heap[smallest].custo) smallest = right;
+
+        if (smallest == index) break;
+        swap(&heap[index], &heap[smallest]);
+        index = smallest;
+    }
+}
+
+void push(No **heap, int *size, int *capacity, No value) {
+    if (*size >= *capacity) {
+        *capacity *= 2;
+        *heap = realloc(*heap, *capacity * sizeof(No));
+        if (*heap == NULL) {
+            fprintf(stderr, "Erro de alocação de memória para o heap\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+    (*heap)[(*size)++] = value;
+    heapifyUp(*heap, *size - 1);
+}
+
+No pop(No *heap, int *size) {
+    No root = heap[0];
+    heap[0] = heap[--(*size)];
+    heapifyDown(heap, *size, 0);
+    return root;
 }
 
 int dijkstraModificado(int n) {
@@ -52,14 +106,17 @@ int dijkstraModificado(int n) {
     }
     dist[0][0] = 0;
 
-    No heap[2 * MAXN];
+    int heapCapacity = 2 * MAXN;
+    No *heap = malloc(heapCapacity * sizeof(No));
+    if (heap == NULL) {
+        fprintf(stderr, "Erro de alocação de memória para o heap\n");
+        exit(EXIT_FAILURE);
+    }
     int heapSize = 0;
-    heap[heapSize++] = (No){0, 0, 0}; // (custo, cidade, paridade)
+    push(&heap, &heapSize, &heapCapacity, (No){0, 0, 0}); 
 
     while (heapSize > 0) {
-        No u = heap[0];
-        heap[0] = heap[--heapSize];
-        qsort(heap, heapSize, sizeof(No), comparador);
+        No u = pop(heap, &heapSize);
 
         if (u.custo > dist[u.cidade][u.paridade])
             continue;
@@ -71,30 +128,38 @@ int dijkstraModificado(int n) {
 
             if (novoCusto < dist[v.cidade][novaParidade]) {
                 dist[v.cidade][novaParidade] = novoCusto;
-                heap[heapSize++] = (No){novoCusto, v.cidade, novaParidade};
-                qsort(heap, heapSize, sizeof(No), comparador);
+                push(&heap, &heapSize, &heapCapacity, (No){novoCusto, v.cidade, novaParidade});
             }
         }
     }
 
-    return dist[n - 1][0] == INF ? -1 : dist[n - 1][0];
+    free(heap);
+
+    int resultado = dist[n - 1][0] == INF ? -1 : dist[n - 1][0];
+    return resultado;
 }
 
 int main() {
     int N, M;
-    scanf("%d %d", &N, &M);
+    if (scanf("%d %d", &N, &M) != 2) {
+        fprintf(stderr, "Erro na leitura da entrada.\n");
+        return 1;
+    }
 
     inicializaGrafo(N);
 
     for (int i = 0; i < M; i++) {
         int c1, c2, g;
-        scanf("%d %d %d", &c1, &c2, &g);
-        adicionaEstrada(c1, c2, g);
+        if (scanf("%d %d %d", &c1, &c2, &g) != 3) {
+            fprintf(stderr, "Erro na leitura de estrada %d.\n", i + 1);
+            return 1;
+        }
+        adicionaEstrada(c1 - 1, c2 - 1, g);
     }
 
     int resultado = dijkstraModificado(N);
     printf("%d\n", resultado);
-
+    
     for (int i = 0; i < N; i++) {
         free(adj[i]);
     }
